a7f3a38eae51dbd453176f656ac1a93493232dda
Adrian Aleksanderek <aleksanderek.adrian@gmail.com>
2025-11-18 16:58:27 +0100

    Adjust Calendar on mobile

--- src/pages/Dashboard/Calendar/CalendarControls.tsx
+++ src/pages/Dashboard/Calendar/CalendarControls.tsx
@@ -6,9 +6,13 @@ import {
   Badge,
   Button,
   Tooltip,
+  useTheme,
+  useMediaQuery,
+  Menu,
+  MenuItem,
 } from '@mui/material';
 import FilterListIcon from '@mui/icons-material/FilterList';
-import { ChevronLeft, ChevronRight } from '@mui/icons-material';
+import { ChevronLeft, ChevronRight, MoreHoriz } from '@mui/icons-material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import type { Dayjs } from 'dayjs';
@@ -30,7 +34,174 @@ export const CalendarControls: React.FC<CalendarControlsProps> = ({
   handleMonthChange,
   handleDatePickerChange,
 }) => {
-  return (
+  const theme = useTheme();
+  const isPhone = useMediaQuery(theme.breakpoints.down('sm'));
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const openMobileMenu = Boolean(anchorEl);
+  const handleClickMobileMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMobileMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const phone = (
+    <Stack
+      direction={'column'}
+      gap={2}
+      mb={1}
+      width={'100%'}
+      className={
+        'border-lightGray rounded-lg border bg-gray-50 px-3 py-3 md:py-2'
+      }
+    >
+      <Stack
+        sx={{ flexGrow: 1 }}
+        alignItems={'center'}
+        direction={'row'}
+        flexWrap={'wrap'}
+        justifyContent={'space-between'}
+      >
+        <Typography
+          textTransform={'capitalize'}
+          className="rounded-full border border-gray-700 px-3 py-1 font-semibold"
+        >
+          {currentMonth.format('MMMM YYYY')}
+        </Typography>
+        <IconButton onClick={handleClickMobileMenu}>
+          <MoreHoriz />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={openMobileMenu}
+          onClose={handleCloseMobileMenu}
+        >
+          <MenuItem
+            disableRipple
+            key={'filters'}
+            onClick={() => {
+              setIsFilterOpen(true);
+            }}
+          >
+            <Badge
+              badgeContent={selectedEmployees.length}
+              color="primary"
+              sx={{ width: '100%' }}
+            >
+              <Button
+                startIcon={<FilterListIcon />}
+                size="small"
+                fullWidth
+                color="primary"
+                className="rounded-lg border"
+                onClick={() => {
+                  setIsFilterOpen(true);
+                }}
+                sx={(theme) => ({
+                  borderColor: theme.palette.primary.light,
+                  '&:hover': {
+                    borderColor: theme.palette.primary.main,
+                  },
+                })}
+              >
+                Filtry
+              </Button>
+            </Badge>
+          </MenuItem>
+          <MenuItem disableRipple key={'datePicker'}>
+            <LocalizationProvider
+              localeText={
+                plPL.components.MuiLocalizationProvider.defaultProps.localeText
+              }
+              dateAdapter={AdapterDayjs}
+              adapterLocale="pl"
+            >
+              <DatePicker
+                openTo="month"
+                views={['year', 'month']}
+                sx={(theme) => ({
+                  minWidth: 200,
+                  '& .MuiPickersSectionList-root': {
+                    padding: '7px 0',
+                    width: 'auto',
+                  },
+                  '&:hover .MuiPickersOutlinedInput-notchedOutline': {
+                    borderColor: `${theme.palette.primary.main} !important`,
+                  },
+                  '& .MuiPickersOutlinedInput-notchedOutline': {
+                    borderColor: theme.palette.primary.light,
+                  },
+                  '& .MuiPickersInputBase-root': {
+                    color: theme.palette.primary.main,
+                    fontWeight: 500,
+                  },
+                  '& .MuiButtonBase-root': {
+                    color: theme.palette.primary.main,
+                  },
+                })}
+                value={currentMonth}
+                onChange={handleDatePickerChange}
+              />
+            </LocalizationProvider>
+          </MenuItem>
+        </Menu>
+      </Stack>
+
+      <Stack direction={'row'}>
+        <Tooltip title={'Poprzedni miesiąc'}>
+          <IconButton
+            size="small"
+            className="rounded-l-lg rounded-r-none border"
+            color="primary"
+            onClick={() => handleMonthChange('prev')}
+            sx={(theme) => ({
+              borderColor: theme.palette.primary.light,
+              '&:hover': {
+                borderColor: theme.palette.primary.main,
+              },
+            })}
+          >
+            <ChevronLeft />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title={'Obecy miesiąc'}>
+          <Button
+            variant="outlined"
+            className="rounded-none border-x-0"
+            color="primary"
+            onClick={() => handleMonthChange('today')}
+            sx={(theme) => ({
+              borderColor: theme.palette.primary.light,
+              flexGrow: 1,
+              '&:hover': {
+                borderColor: theme.palette.primary.main,
+              },
+            })}
+          >
+            Dziś
+          </Button>
+        </Tooltip>
+        <Tooltip title={'Następny miesiąc'}>
+          <IconButton
+            size="small"
+            color="primary"
+            className="rounded-l-none rounded-r-lg border"
+            onClick={() => handleMonthChange('next')}
+            sx={(theme) => ({
+              borderColor: theme.palette.primary.light,
+              '&:hover': {
+                borderColor: theme.palette.primary.main,
+              },
+            })}
+          >
+            <ChevronRight />
+          </IconButton>
+        </Tooltip>
+      </Stack>
+    </Stack>
+  );
+
+  const desktop = (
     <Stack
       alignItems={'center'}
       direction={'row'}
@@ -147,27 +318,6 @@ export const CalendarControls: React.FC<CalendarControlsProps> = ({
         </Badge>
       </Tooltip>
 
-      {/* <Stack
-                sx={{ flexGrow: 1 }}
-                alignItems={'center'}
-                direction={'row'}
-                flexWrap={'wrap'}
-                justifyContent={{ xs: 'flex-start', md: 'flex-end' }}
-              >
-                <Typography
-                  className="rounded-full border border-gray-700 px-3 py-1 font-semibold"
-                  sx={{
-                    display: {
-                      xs: 'none',
-                      sm: 'block',
-                    },
-                  }}
-                >
-                  {dayjs(fromWeek).format('DD.MM.YY')} -{' '}
-                  {dayjs(toWeek).add(6, 'day').format('DD.MM.YY')}
-                </Typography>
-              </Stack> */}
-
       <Stack
         sx={{ flexGrow: 1 }}
         alignItems={'center'}
@@ -184,4 +334,6 @@ export const CalendarControls: React.FC<CalendarControlsProps> = ({
       </Stack>
     </Stack>
   );
+
+  return isPhone ? phone : desktop;
 };
