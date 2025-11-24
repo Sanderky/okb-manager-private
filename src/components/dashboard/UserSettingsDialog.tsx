58db256df4eb073a5f7e595e61a4567365de8008
Adrian Aleksanderek <aleksanderek.adrian@gmail.com>
2025-11-24 01:02:55 +0100

    Improve User Settings

--- src/components/dashboard/UserSettingsDialog.tsx
+++ src/components/dashboard/UserSettingsDialog.tsx
@@ -9,14 +9,14 @@ import {
   InputAdornment,
   Collapse,
 } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { OutgoingMail, Visibility, VisibilityOff } from '@mui/icons-material';
 import {
   updatePassword,
-  updateEmail,
   updateProfile,
   EmailAuthProvider,
   reauthenticateWithCredential,
   type User,
+  verifyBeforeUpdateEmail,
 } from 'firebase/auth';
 import { auth } from '../../firebase';
 import BaseDialog from '../BaseDialog';
@@ -34,7 +34,9 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
   const user = auth.currentUser;
   const notifications = useNotifications();
 
-  const [editMode, setEditMode] = useState(false);
+  const [emailEditMode, setEmailEditMode] = useState(false);
+  const [usernameEditMode, setUsernameEditMode] = useState(false);
+  const [verificationEmailInfo, setVerificationEmailInfo] = useState(false);
 
   const [displayName, setDisplayName] = useState('');
   const [email, setEmail] = useState('');
@@ -58,9 +60,9 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
       setCurrentPassword('');
       setNewPassword('');
       setConfirmPassword('');
-      // setError('');
-      setEditMode(false);
-      // setSuccess('');
+      setEmailEditMode(false);
+      setUsernameEditMode(false);
+      setVerificationEmailInfo(false);
       setFieldsError({});
     }
   }, [user, open]);
@@ -123,7 +125,7 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
 
     if (result && user) {
       if (await reauthenticate(password)) {
-        await updateEmail(user, email.trim());
+        await verifyBeforeUpdateEmail(user, email.trim());
       } else {
         setFieldsError((prev) => ({
           ...prev,
@@ -183,7 +185,42 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
     return result;
   };
 
-  const handleSave = async (): Promise<void> => {
+  const handleSaveEmail = async (): Promise<void> => {
+    if (!user) {
+      notifications.show('Użytkownik nie jest zalogowany', {
+        severity: 'error',
+        autoHideDuration: 5000,
+      });
+      return;
+    }
+
+    setLoading(true);
+    setFieldsError({});
+
+    try {
+      const hasEmailChanged = email !== user.email;
+      if (!hasEmailChanged) {
+        return;
+      }
+
+      if (await updateUserEmail()) {
+        setVerificationEmailInfo(true);
+        setEmailEditMode(false);
+      }
+    } catch (error) {
+      setEmailEditMode(false);
+      setEmail(user.email ?? '');
+      console.error('Error updating user:', error);
+      notifications.show('Wystąpił błąd podczas aktualizacji danych', {
+        severity: 'error',
+        autoHideDuration: 5000,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSaveUsername = async (): Promise<void> => {
     if (!user) {
       notifications.show('Użytkownik nie jest zalogowany', {
         severity: 'error',
@@ -197,25 +234,16 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
 
     try {
       const hasDisplayNameChanged = displayName !== user.displayName;
-      const hasEmailChanged = email !== user.email;
-      const hasPasswordChanged = !!newPassword;
-      let result = false;
 
-      if (!hasDisplayNameChanged && !hasEmailChanged && !hasPasswordChanged) {
+      if (!hasDisplayNameChanged) {
         return;
       }
 
-      if (hasDisplayNameChanged && (await updateDisplayName())) result = true;
-
-      if (hasEmailChanged && (await updateUserEmail())) result = true;
-
-      if (result) {
-        notifications.show('Pomyślnie zaktualizowano dane użytkownika', {
+      if (await updateDisplayName()) {
+        notifications.show('Pomyślnie zaktualizowano nazwę użytkownika', {
           severity: 'success',
           autoHideDuration: 5000,
         });
-        setPassword('');
-        setEditMode(false);
         handleClose();
       }
     } catch (error) {
@@ -226,6 +254,7 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
       });
     } finally {
       setLoading(false);
+      setUsernameEditMode(false);
     }
   };
 
@@ -266,6 +295,7 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
   };
 
   const handleClose = (): void => {
+    // setVerificationEmailInfo(false)
     onClose();
   };
 
@@ -280,65 +310,264 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
       title="Ustawienia konta"
       showConfirm={false}
     >
-      <Box component="form" sx={{ mt: 1 }}>
-        <Typography variant="body1" gutterBottom>
-          Informacje podstawowe
-        </Typography>
+      {verificationEmailInfo ? (
+        <Box component="form" sx={{ mt: 1, textAlign: 'center' }}>
+          <OutgoingMail
+            color="primary"
+            sx={{
+              fontSize: '8rem',
+            }}
+          />
+          <Typography
+            sx={{
+              fontWeight: 500,
+              fontSize: '1.5rem',
+            }}
+          >
+            Weryfikacja adresu email
+          </Typography>
+          <Typography>
+            Na podany adres email został wysłany link aktywacyjny.
+          </Typography>
+          <Typography mt={4} mb={2}>
+            Potwierdź nowego maila, aby dokonać zmiany.
+          </Typography>
+        </Box>
+      ) : (
+        <Box component="form" sx={{ mt: 1 }}>
+          <Typography variant="body1" gutterBottom>
+            Informacje podstawowe
+          </Typography>
 
-        <TextField
-          size="small"
-          label="Nazwa użytkownika"
-          slotProps={{
-            input: {
-              readOnly: !editMode,
-            },
-          }}
-          value={displayName}
-          onChange={(e) => setDisplayName(e.target.value)}
-          fullWidth
-          margin="normal"
-          disabled={loading || !editMode}
-        />
-
-        <TextField
-          size="small"
-          label="Email"
-          type="email"
-          slotProps={{
-            input: {
-              readOnly: !editMode,
-            },
-          }}
-          error={Boolean(fieldsErrors['email'])}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          fullWidth
-          helperText={fieldsErrors['email']}
-          margin="normal"
-          disabled={loading || !editMode}
-        />
-
-        <Collapse in={editMode}>
           <TextField
             size="small"
-            label="Hasło"
-            error={Boolean(fieldsErrors['password'])}
-            type={showPassword ? 'text' : 'password'}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            label="Nazwa użytkownika"
+            slotProps={{
+              input: {
+                readOnly: !usernameEditMode,
+              },
+            }}
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+            fullWidth
+            margin="normal"
+            disabled={loading || !usernameEditMode}
+          />
+
+          <Box
+            sx={{
+              mt: 1,
+              mb: 2,
+            }}
+          >
+            {!usernameEditMode && (
+              <Button
+                variant="contained"
+                onClick={() => setUsernameEditMode(true)}
+              >
+                Edytuj nazwę użytkownika
+              </Button>
+            )}
+            {usernameEditMode && (
+              <>
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  className="border-gray-400"
+                  sx={{ mr: 2 }}
+                  onClick={() => {
+                    setDisplayName(user.displayName || '');
+                    setUsernameEditMode(false);
+                  }}
+                >
+                  Anuluj
+                </Button>
+
+                <Button
+                  variant="contained"
+                  onClick={handleSaveUsername}
+                  loading={loading}
+                >
+                  Zapisz
+                </Button>
+              </>
+            )}
+          </Box>
+
+          <TextField
+            size="small"
+            label="Email"
+            type="email"
+            slotProps={{
+              input: {
+                readOnly: !emailEditMode,
+              },
+            }}
+            error={Boolean(fieldsErrors['email'])}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+            helperText={fieldsErrors['email']}
+            margin="normal"
+            disabled={loading || !emailEditMode}
+          />
+
+          <Collapse in={emailEditMode}>
+            <TextField
+              size="small"
+              label="Hasło"
+              error={Boolean(fieldsErrors['password'])}
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+              margin="normal"
+              disabled={loading}
+              helperText={fieldsErrors['password']}
+              slotProps={{
+                input: {
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setShowPassword((prev) => !prev)}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                },
+              }}
+            />
+
+            <Typography
+              variant="caption"
+              color="text.secondary"
+              display="block"
+              sx={{ mt: 1, mb: 2 }}
+            >
+              Aby zmienić adres email należy również wprowadzić obecne hasło
+            </Typography>
+          </Collapse>
+
+          {!emailEditMode && (
+            <Button
+              variant="contained"
+              sx={{ mt: 1 }}
+              onClick={() => setEmailEditMode(true)}
+            >
+              Edytuj adres email
+            </Button>
+          )}
+          {emailEditMode && (
+            <>
+              <Button
+                variant="outlined"
+                color="inherit"
+                className="border-gray-400"
+                sx={{ mr: 2 }}
+                onClick={() => {
+                  setEmail(user.email || '');
+                  setEmailEditMode(false);
+                }}
+              >
+                Anuluj
+              </Button>
+
+              <Button
+                variant="contained"
+                onClick={handleSaveEmail}
+                loading={loading}
+              >
+                Zapisz
+              </Button>
+            </>
+          )}
+
+          <Divider sx={{ my: 3 }} />
+
+          <Typography variant="body1" gutterBottom>
+            Zmiana hasła
+          </Typography>
+
+          <TextField
+            size="small"
+            label="Obecne hasło"
+            error={Boolean(fieldsErrors['currentPassword'])}
+            type={showCurrentPassword ? 'text' : 'password'}
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
             fullWidth
             margin="normal"
             disabled={loading}
-            helperText={fieldsErrors['password']}
+            helperText={fieldsErrors['currentPassword']}
             slotProps={{
               input: {
                 endAdornment: (
                   <InputAdornment position="end">
                     <IconButton
-                      onClick={() => setShowPassword((prev) => !prev)}
+                      onClick={() =>
+                        setShowCurrentPassword(!showCurrentPassword)
+                      }
                       edge="end"
                     >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                      {showCurrentPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+
+          <TextField
+            label="Nowe hasło"
+            size="small"
+            error={Boolean(fieldsErrors['newPassword'])}
+            helperText={fieldsErrors['newPassword']}
+            type={showNewPassword ? 'text' : 'password'}
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            fullWidth
+            margin="normal"
+            disabled={loading}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() => setShowNewPassword(!showNewPassword)}
+                      edge="end"
+                    >
+                      {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+
+          <TextField
+            label="Potwierdź nowe hasło"
+            size="small"
+            error={Boolean(fieldsErrors['confirmPassword'])}
+            helperText={fieldsErrors['confirmPassword']}
+            type={showConfirmPassword ? 'text' : 'password'}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            fullWidth
+            margin="normal"
+            disabled={loading}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={() =>
+                        setShowConfirmPassword(!showConfirmPassword)
+                      }
+                      edge="end"
+                    >
+                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
                     </IconButton>
                   </InputAdornment>
                 ),
@@ -352,141 +581,18 @@ const UserSettingsDialog: React.FC<UserSettingsDialogProps> = ({
             display="block"
             sx={{ mt: 1, mb: 2 }}
           >
-            Aby zmienić adres email należy również wprowadzić obecne hasło
+            Hasło musi mieć co najmniej 8 znaków
           </Typography>
-        </Collapse>
 
-        {!editMode && (
           <Button
             variant="contained"
-            sx={{ mt: 2 }}
-            onClick={() => setEditMode(true)}
+            onClick={handleResetPassword}
+            loading={loading}
           >
-            Edytuj
+            Resetuj hasło
           </Button>
-        )}
-        {editMode && (
-          <>
-            <Button
-              variant="outlined"
-              color="inherit"
-              className="border-gray-400"
-              sx={{ mr: 2 }}
-              onClick={() => setEditMode(false)}
-            >
-              Anuluj
-            </Button>
-
-            <Button variant="contained" onClick={handleSave} loading={loading}>
-              Zapisz
-            </Button>
-          </>
-        )}
-
-        <Divider sx={{ my: 3 }} />
-
-        <Typography variant="body1" gutterBottom>
-          Zmiana hasła
-        </Typography>
-
-        <TextField
-          size="small"
-          label="Obecne hasło"
-          error={Boolean(fieldsErrors['currentPassword'])}
-          type={showCurrentPassword ? 'text' : 'password'}
-          value={currentPassword}
-          onChange={(e) => setCurrentPassword(e.target.value)}
-          fullWidth
-          margin="normal"
-          disabled={loading}
-          helperText={fieldsErrors['currentPassword']}
-          slotProps={{
-            input: {
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => setShowCurrentPassword(!showCurrentPassword)}
-                    edge="end"
-                  >
-                    {showCurrentPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            },
-          }}
-        />
-
-        <TextField
-          label="Nowe hasło"
-          size="small"
-          error={Boolean(fieldsErrors['newPassword'])}
-          helperText={fieldsErrors['newPassword']}
-          type={showNewPassword ? 'text' : 'password'}
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          fullWidth
-          margin="normal"
-          disabled={loading}
-          slotProps={{
-            input: {
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => setShowNewPassword(!showNewPassword)}
-                    edge="end"
-                  >
-                    {showNewPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            },
-          }}
-        />
-
-        <TextField
-          label="Potwierdź nowe hasło"
-          size="small"
-          error={Boolean(fieldsErrors['confirmPassword'])}
-          helperText={fieldsErrors['confirmPassword']}
-          type={showConfirmPassword ? 'text' : 'password'}
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          fullWidth
-          margin="normal"
-          disabled={loading}
-          slotProps={{
-            input: {
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                    edge="end"
-                  >
-                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            },
-          }}
-        />
-
-        <Typography
-          variant="caption"
-          color="text.secondary"
-          display="block"
-          sx={{ mt: 1, mb: 2 }}
-        >
-          Hasło musi mieć co najmniej 8 znaków
-        </Typography>
-
-        <Button
-          variant="contained"
-          onClick={handleResetPassword}
-          loading={loading}
-        >
-          Resetuj hasło
-        </Button>
-      </Box>
+        </Box>
+      )}
     </BaseDialog>
   );
 };
