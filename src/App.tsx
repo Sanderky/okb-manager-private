c5cbd7b41d9fe387cae20546a6574f54efea0f6f
Adrian Aleksanderek <aleksanderek.adrian@gmail.com>
2025-11-24 16:57:07 +0100

    Change autoselect no options text

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,11 @@ const customTheme = createTheme({
         },
       },
     },
+    MuiAutocomplete: {
+      defaultProps: {
+        noOptionsText: 'Brak danych',
+      },
+    },
   },
 });
 
